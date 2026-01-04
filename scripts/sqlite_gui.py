7678ed6e7f6ffc624e26dfd265e9904fcdefcb0c
Saurav Sengupta <69458015+sauravq405@users.noreply.github.com>
2026-01-04 19:36:18 +0530

    Update sqlite_gui.py

--- scripts/sqlite_gui.py
+++ scripts/sqlite_gui.py
@@ -22,6 +22,25 @@ Run the app:
 ------------
 streamlit run sqlite_gui.py --server.address 0.0.0.0 --server.port 8501
 
+Run the app in detached mode:
+------------
+nohup streamlit run sqlite_gui.py --server.address 0.0.0.0 --server.port 8501 > streamlit.log 2>&1 &
+
+To kill it:
+------------
+Find the Process ID (PID):
+
+ps aux | grep streamlit
+
+Example:
+USER	PID	%CPU	%MEM	COMMAND
+holiday	16288	3.2	1.1	/home/holiday/myenv/bin/python3 ... streamlit ...
+
+Kill the process:
+------------
+kill -9 <PID>
+
+
 Access from local Windows browser:
 ---------------------------------
 http://<VM-IP>:8501
